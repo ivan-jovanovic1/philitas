658d5fd921b84afbce7d699da5819122dc778911
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-03-29 23:30:40 +0200

    Remove redundant dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import Express, { Application, Request, Response, NextFunction } from "express";
 import session from "express-session";
-import httpError from "http-errors";
-import path from "path";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import ConnectMongoDBStore, {
-  MongoDBSessionOptions,
-} from "connect-mongodb-session";
+
 import cors from "cors";
 import dotenv from "dotenv";
 
 import UserRouter from "./routes/UserRouter";
 import WordRouter from "./routes/WordRouter";
 
-const MongoDBStore = ConnectMongoDBStore(session);
 /// Environment variables
 declare var process: {
   env: {
