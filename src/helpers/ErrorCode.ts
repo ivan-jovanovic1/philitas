e2747d6f3b5678adcf94c833cc4a72d92a99482f
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-04-04 00:20:22 +0200

    Update ErrorCode.ts

--- src/helpers/ErrorCode.ts
+++ src/helpers/ErrorCode.ts
@@ -1,4 +1,5 @@
 export namespace ErrorCode {
-  export const undefinedData = 10002;
   export const expiredData = 10001;
+  export const undefinedData = 10002;
+  export const notFoundData = 10003;
 }
