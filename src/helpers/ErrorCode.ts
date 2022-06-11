036908e704e6ed0722cb184f92936a11be5b6af2
ivan-jovanovic1 <ivan.jovanovic@mediately.co>
2022-06-11 19:19:51 +0200

    Add takenUsername error code.

--- src/helpers/ErrorCode.ts
+++ src/helpers/ErrorCode.ts
@@ -3,4 +3,5 @@ export namespace ErrorCode {
   export const undefinedData = 10002;
   export const notFoundData = 10003;
   export const unableToAddToFavorites = 10004;
+  export const takenUsername = 10005;
 }
